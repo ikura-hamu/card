bfdf7567ada6b76cf5948859d549ba563d5ea004
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-07-22 00:45:49 +0900

    :sparkles: md表示

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+	"io"
+	"net/http"
 
-type model struct{}
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+)
+
+type model struct {
+	readMeContent string
+	viewport      viewport.Model
+	ready         bool
+}
+
+type errMsg struct {
+	err error
+}
+
+type readMeMsg struct {
+	content string
+}
+
+func fetchReadme() tea.Msg {
+	resp, err := http.Get("https://raw.githubusercontent.com/ikura-hamu/ikura-hamu/refs/heads/main/README.md")
+	if err != nil {
+		return errMsg{err: fmt.Errorf("fetch readme: %w", err)}
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return errMsg{err: fmt.Errorf("fetch readme: %w", err)}
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return errMsg{err: fmt.Errorf("read readme: %w", err)}
+	}
+
+	return readMeMsg{content: string(body)}
+}
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return fetchReadme
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -15,12 +54,39 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
+	case errMsg:
+		return m, tea.Quit
+	case readMeMsg:
+		m.readMeContent = msg.content
+		md, err := glamour.Render(m.readMeContent, "dark")
+		if err != nil {
+			return m, tea.Quit
+		}
+		m.viewport.SetContent(md)
+	case tea.WindowSizeMsg:
+		if !m.ready {
+			m.viewport = viewport.New(msg.Width, msg.Height)
+			m.viewport.SetContent(m.readMeContent)
+			m.ready = true
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height
+		}
+
 	}
-	return m, nil
+
+	var cmd tea.Cmd
+	m.viewport, cmd = m.viewport.Update(msg)
+
+	return m, cmd
 }
 
 func (m model) View() string {
-	return "Press 'q' to quit.\n"
+	if m.readMeContent == "" {
+		return "Loading README...\n"
+	}
+
+	return m.viewport.View()
 }
 
 func main() {
